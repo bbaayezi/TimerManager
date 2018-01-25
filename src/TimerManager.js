270fc7f58755c675f314bff46ff074980745b363
robin <476296987@qq.com>
2018-01-25 22:12:24 +0800

    alpha version

--- src/TimerManager.js
+++ src/TimerManager.js
@@ -1,6 +1,53 @@
-class TimerManager {
-    
+export default class TimerManager {
+    timers = [];
+    createTimer (name, options = {
+        type = string,
+        handler = null,
+        timeout = number
+    } = {}) {
+        switch (options.type) {
+            case "interval":
+            this.timers.push({name, timeout: options.timeout, type: options.type, observer: window.setInterval(options.handler, options.timeout), handler: options.handler})
+            break;
+            case "timeout":
+            this.timers.push({name, timeout: options.timeout, type: options.type, observer: window.setTimeout(options.handler, options.timeout), handler: options.handler})
+            break;
+        }
+    }
 
+    clearTimer (name) {
+        for (let i = 0; i < this.timers.length; i++) {
+            if (this.timers[i].name == name && this.timers[i].type == "interval") {
+                window.clearInterval(this.timers[i].observer);
+                this.timers.splice(i, 1);
+            } else if (this.timers[i].name == name && this.timers[i].type == "timeout") {
+                window.clearTimeout(this.timers[i].observer);
+                this.timers.splice(i, 1);
+            }
+        }
+    }
+
+    pauseTimer (name) {
+        for (let i = 0; i < this.timers.length; i++) {
+            if (this.timers[i].name == name && this.timers[i].type == "interval") {
+                window.clearInterval(this.timers[i].observer);
+            } else if (this.timers[i].name == name && this.timers[i].type == "timeout") {
+                window.clearTimeout(this.timers[i].observer);
+            }
+        }
+    }
+
+    restartTimer (name) {
+        for (let i = 0; i < this.timers.length; i++) {
+            if (this.timers[i].name == name && this.timers[i].type == "interval") {
+                this.timers[i].observer = window.setInterval(this.timers[i].handler, this.timers[i].timeout);
+            } else if (this.timers[i].name == name && this.timers[i].type == "timeout") {
+                this.timers[i].observer = window.setTimeout(this.timers[i].handler, this.timers[i].timeout);
+            }
+        }
+    }
+
+    listTimers () {
+        return this.timers;
+    }
 }
-
-export default TimerManager;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,37 @@
 import TimerManager from './TimerManager';
 
-console.log('test');
+let tm = new TimerManager();
+
+tm.createTimer ("timeout 1", {
+    type: "interval",
+    handler: () => {
+        console.log('interval!')
+    },
+    timeout: 2000
+})
+
+tm.createTimer ("timeout 2", {
+    type: "timeout",
+    handler: () => {
+        console.log('time out!');
+        tm.clearTimer("timeout 1");
+        console.log(tm.listTimers());
+    },
+    timeout: 3000
+})
+
+tm.createTimer("interval 1", {
+    type: "interval",
+    handler: () => {
+        console.log("Interval 1!");
+    },
+    timeout: 2000
+})
+
+tm.pauseTimer("interval 1");
+
+setTimeout(() => {
+    tm.restartTimer("interval 1");
+    console.log(tm.listTimers());
+}, 3000);
+
